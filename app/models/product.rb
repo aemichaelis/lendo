3886cad857849278a899d73659632e31ee7672df
Paul Hemmingson <paul-hemmingson@t-online.de>
2020-12-01 12:38:30 +0100

    update the confirmation

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,19 @@ class Product < ApplicationRecord
   condition = ['Like New', 'Normal Wear', 'Minor Cosmetic Faults', 'Minor Functional Faults']
   belongs_to :user
   has_many :bookings
+
+  def unavailable_dates
+    bookings.pluck(:check_in, :check_out).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
+
   has_many :favourites
   has_many :reviews, through: :bookings, :dependent => :destroy
   has_many_attached :photos
   validates :title, :description, :address, :price, :model, :brand, presence: true
   validates :delivery_method, presence: true, inclusion: { in: ['Pick up', 'Delivery', 'Flexible'] }
   validates :category, presence: true, inclusion: { in: categories }
+
+
 end
